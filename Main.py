43e5e45d168f55afda7279dde1550497087c5ba7
Rafael Xavier <rxaviermontero@gmail.com>
2019-04-02 01:04:17 -0300

    Transform into functions

--- Main.py
+++ Main.py
@@ -15,19 +15,35 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 num = 10
 
-for user in candidates.values():
-    try:
-        with open("Pickles/" + user + ".p", "rb") as data_load:
-            data = pickle.load(data_load)
-        print("Previous data found for " + user + ", downloading last " + str(num) + " tweets since tweet ID " +
-              str(data[0][1]) + ": " + data[0][0])
-        Sent.sent(api, user, num, data)
-    except IOError:
-        print("No previous data found for " + user + ", downloading last " + str(num) + " tweets")
-        data = []
-        Sent.sent(api, user, num, data)
 
-full_data = {}
-for user in candidates.values():
-    with open("Pickles/" + user + ".p", "rb") as dl:
-        full_data.update({user: pickle.load(dl)})
+def get_latest(user_dict):
+    for user in user_dict.values():
+        try:
+            with open("Pickles/" + user + ".p", "rb") as data_load:
+                data = pickle.load(data_load)
+            print("Previous data found for " + user + ", downloading last " + str(num) + " tweets since tweet ID " +
+                  str(data[0][1]) + ": " + data[0][0])
+            Sent.sent_latest(api, user, num, data)
+        except IOError:
+            print("No previous data found for " + user + ", downloading last " + str(num) + " tweets")
+            data = []
+            Sent.sent_latest(api, user, num, data)
+
+
+def build_data(user_dict):
+    full_data = {}
+    for user in user_dict.values():
+        with open("Pickles/" + user + ".p", "rb") as dl:
+            full_data.update({user: pickle.load(dl)})
+
+
+def get_previous(user_dict):
+    for user in user_dict.values():
+        try:
+            with open("Pickles/" + user + ".p", "rb") as data_load:
+                data = pickle.load(data_load)
+            print("Successfully loaded data for " + user + ", downloading " + str(num) + " tweets prior to tweet ID " +
+                  str(data[len(data)-1][1]) + ": " + data[len(data)-1][0])
+            Sent.sent_previous(api, user, num, data)
+        except IOError:
+            print("Download some data first with get_latest function")
