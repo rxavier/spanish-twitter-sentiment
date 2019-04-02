6b6727c4eb49fa7a6e0d12663d8ac6fc4bdc77b1
Rafael Xavier <rxaviermontero@gmail.com>
2019-04-02 20:38:05 -0300

    Rename functions

--- Main.py
+++ Main.py
@@ -13,21 +13,31 @@ auth = tweepy.OAuthHandler(keys["consumer_token"], keys["consumer_token_secret"]
 auth.set_access_token(keys["access_token"], keys["access_token_secret"])
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-num = 10
 
-
-def get_latest(user_dict):
+def get_latest(user_dict, number_tweets):
     for user in user_dict.values():
         try:
             with open("Pickles/" + user + ".p", "rb") as data_load:
                 data = pickle.load(data_load)
-            print("Previous data found for " + user + ", downloading last " + str(num) + " tweets since tweet ID " +
-                  str(data[0][1]) + ": " + data[0][0])
-            Sent.sent_latest(api, user, num, data)
+            print("Previous data found for " + user + ", downloading last " + str(number_tweets) +
+                  " tweets since tweet ID " + str(data[0][1]) + ": " + data[0][0])
+            Sent.sent_latest(api, user, number_tweets, data)
         except IOError:
-            print("No previous data found for " + user + ", downloading last " + str(num) + " tweets")
+            print("No previous data found for " + user + ", downloading last " + str(number_tweets) + " tweets")
             data = []
-            Sent.sent_latest(api, user, num, data)
+            Sent.sent_latest(api, user, number_tweets, data)
+
+
+def get_previous(user_dict, number_tweets):
+    for user in user_dict.values():
+        try:
+            with open("Pickles/" + user + ".p", "rb") as data_load:
+                data = pickle.load(data_load)
+            print("Successfully loaded data for " + user + ", downloading " + str(number_tweets) +
+                  " tweets prior to tweet ID " + str(data[len(data)-1][1]) + ": " + data[len(data)-1][0])
+            Sent.sent_previous(api, user, number_tweets, data)
+        except IOError:
+            print("Download some data first with get_latest function")
 
 
 def build_data(user_dict):
@@ -36,15 +46,3 @@ def build_data(user_dict):
         with open("Pickles/" + user + ".p", "rb") as dl:
             full_data.update({user: pickle.load(dl)})
     return full_data
-
-
-def get_previous(user_dict):
-    for user in user_dict.values():
-        try:
-            with open("Pickles/" + user + ".p", "rb") as data_load:
-                data = pickle.load(data_load)
-            print("Successfully loaded data for " + user + ", downloading " + str(num) + " tweets prior to tweet ID " +
-                  str(data[len(data)-1][1]) + ": " + data[len(data)-1][0])
-            Sent.sent_previous(api, user, num, data)
-        except IOError:
-            print("Download some data first with get_latest function")
