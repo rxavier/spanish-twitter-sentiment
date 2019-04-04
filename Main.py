bacec17e334b9b5ea18c5aecc8810c291bcb1875
Rafael Xavier <rxaviermontero@gmail.com>
2019-04-04 17:52:32 -0300

    Fix pickle filenames

--- Main.py
+++ Main.py
@@ -14,7 +14,7 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 def tweets_replies_last_loop(user_list, number_tweets):
     for user in user_list:
         try:
-            with open("Pickles/tweets_replies" + user + ".p", "rb") as data_load:
+            with open("Pickles/tweets_replies_" + user + ".p", "rb") as data_load:
                 tweets_data = pickle.load(data_load)
             print("Previous tweets found for " + user + ", downloading last " + str(number_tweets) +
                   " tweets since tweet ID " + str(tweets_data[0][1]) + ": " + tweets_data[0][0])
@@ -28,7 +28,7 @@ def tweets_replies_last_loop(user_list, number_tweets):
 def tweets_replies_previous_loop(user_list, number_tweets):
     for user in user_list:
         try:
-            with open("Pickles/tweets_replies" + user + ".p", "rb") as data_load:
+            with open("Pickles/tweets_replies_" + user + ".p", "rb") as data_load:
                 tweets_data = pickle.load(data_load)
             print("Successfully loaded tweets for " + user + ", downloading " + str(number_tweets) +
                   " tweets prior to tweet ID " + str(tweets_data[len(tweets_data)-1][1]) + ": " +
@@ -41,7 +41,7 @@ def tweets_replies_previous_loop(user_list, number_tweets):
 def replies_last_loop(user_list, number_replies):
     for user in user_list:
         try:
-            with open("Pickles/replies" + user + ".p", "rb") as data_load:
+            with open("Pickles/replies_" + user + ".p", "rb") as data_load:
                 replies_data = pickle.load(data_load)
             print("Previous replies found for " + user + ", downloading last " + str(number_replies) +
                   " replies since tweet ID " + str(replies_data[0][2]) + ": " + replies_data[0][1])
@@ -49,13 +49,13 @@ def replies_last_loop(user_list, number_replies):
         except IOError:
             print("No previous replies found for " + user + ", downloading last " + str(number_replies) + " replies")
             replies_data = []
-            Sent.tweets_replies_last(api, user, number_replies, replies_data)
+            Sent.replies_last(api, user, number_replies, replies_data)
 
 
 def replies_previous_loop(user_list, number_replies):
     for user in user_list:
         try:
-            with open("Pickles/replies" + user + ".p", "rb") as data_load:
+            with open("Pickles/replies_" + user + ".p", "rb") as data_load:
                 replies_data = pickle.load(data_load)
             print("Successfully loaded replies for " + user + ", downloading " + str(number_replies) +
                   " replies prior to tweet ID " + str(replies_data[len(replies_data) - 1][2]) + ": " +
@@ -68,7 +68,7 @@ def replies_previous_loop(user_list, number_replies):
 def build_tweets(user_list):
     full_tweets_data = {}
     for user in user_list:
-        with open("Pickles/tweets_replies" + user + ".p", "rb") as dl:
+        with open("Pickles/tweets_replies_" + user + ".p", "rb") as dl:
             full_tweets_data.update({user: pickle.load(dl)})
     return full_tweets_data
 
@@ -76,6 +76,6 @@ def build_tweets(user_list):
 def build_replies(user_list):
     full_replies_data = {}
     for user in user_list:
-        with open("Pickles/replies" + user + ".p", "rb") as dl:
+        with open("Pickles/replies_" + user + ".p", "rb") as dl:
             full_replies_data.update({user: pickle.load(dl)})
     return full_replies_data
